3d55032b871055c48d43d1c971449d8e299635e1
kurskov <dmitrii@kurskov.ru>
2023-04-19 15:33:22 +0300

    Lesson 2.2 T

--- Yandex/2.2/t.py
+++ Yandex/2.2/t.py
@@ -2,14 +2,21 @@ text1 = input()
 text2 = input()
 text3 = input()
 
+text = ""
+sl = len(text)
+
 if "зайка" in text1:
     sl = len(text1)
     text = text1
-if ("зайка" in text2) and (len(text2) < sl):
-    sl = len(text2)
-    text = text2
-if "зайка" in text3 and (len(text3) < sl):
-    sl = len(text3)
-    text = text3
 
-print(text + " " + str(sl))
+if "зайка" in text2:
+    if sl == 0 or text2 < text:
+        sl = len(text2)
+        text = text2
+    
+if "зайка" in text3:
+    if sl == 0 or text3 < text:
+        sl = len(text3)
+        text = text3
+
+print(text, str(sl))
