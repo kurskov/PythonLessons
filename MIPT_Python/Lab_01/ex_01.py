4abc10ada8a1f26d635a3d78d00773bd46f6b598
kurskov <dmitrii@kurskov.ru>
2023-06-30 14:57:03 +0300

    Correct with PEP 8

--- MIPT_Python/Lab_01/ex_01.py
+++ MIPT_Python/Lab_01/ex_01.py
@@ -7,6 +7,6 @@
 import numpy as np
 
 for x in 1, 10, 1000:
-    y = (np.log(np.e**(1 / (np.sin(x) + 1)) / (5/4 + 1/x**15)) /
-         np.log(1 + x**2))
+    y = np.log(np.e ** (1 / (np.sin(x) + 1)) / (5 / 4 + 1 / x**15)) / \
+     np.log(1 + x**2)
     print(y)
