8c5b36fa3778c8ed0dd265f3357f000f22311e77
kurskov <dmitrii@kurskov.ru>
2023-07-25 15:11:11 +0300

    Tasks 09 - 14

--- MIPT_CS_Python3/Lab_02/task_12.py
+++ MIPT_CS_Python3/Lab_02/task_12.py
@@ -5,8 +5,14 @@ from pyrob.api import *
 
 @task
 def task_8_6():
-    pass
 
+    while not wall_is_on_the_right():
+        if not wall_is_above() and wall_is_beneath():
+            fill_cell()
+        move_right()
+    
+    if not wall_is_above() and wall_is_beneath():
+        fill_cell()
 
 if __name__ == '__main__':
     run_tasks()
