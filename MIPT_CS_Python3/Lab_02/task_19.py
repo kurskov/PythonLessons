5455c7b9eeca5e24a15237a529b9c52f92faf413
kurskov <dmitrii@kurskov.ru>
2023-07-25 16:14:30 +0300

    Tasks 15 - 19

--- MIPT_CS_Python3/Lab_02/task_19.py
+++ MIPT_CS_Python3/Lab_02/task_19.py
@@ -5,7 +5,19 @@ from pyrob.api import *
 
 @task
 def task_8_29():
-    pass
+    while not wall_is_on_the_left():
+        move_left()
+    if not wall_is_above():
+        while not wall_is_above():
+            move_up()
+    else:
+        while not wall_is_on_the_right():
+            move_right()
+        if not wall_is_above():
+            while not wall_is_above():
+                move_up()
+            while not wall_is_on_the_left():
+                move_left()
 
 
 if __name__ == '__main__':
