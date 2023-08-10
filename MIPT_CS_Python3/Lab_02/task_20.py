64331466f22a25464512461e28efc9495e39f71b
kurskov <dmitrii@kurskov.ru>
2023-08-10 15:56:29 +0300

    Task 20

--- MIPT_CS_Python3/Lab_02/task_20.py
+++ MIPT_CS_Python3/Lab_02/task_20.py
@@ -5,7 +5,16 @@ from pyrob.api import *
 
 @task(delay=0.05)
 def task_4_3():
-    pass
+    move_right()
+    for i in range(6):
+        for j in range(27):
+            fill_cell()
+            move_right()
+        move_down()
+        for j in range(27):
+            move_left()
+            fill_cell()
+        move_down()
 
 
 if __name__ == '__main__':
