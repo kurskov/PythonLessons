b4daae997f9edf5471753e61e84b388ff41393e8
kurskov <dmitrii@kurskov.ru>
2023-09-17 17:35:39 +0300

    Lesson 3.5 project S

--- Yandex_Handbook/3.5/s.py
+++ Yandex_Handbook/3.5/s.py
@@ -1,18 +1,37 @@
-# pitch = input()
+pitch = int(input())
 
-pitch = 3
+upper_limits = {"low": 65, "hi": 91}
+lower_limits = {"low": 97, "hi": 123}
 
-upper_low_limit = 65
-upper_high_limit = 91
-lower_low_limit = 97
-lower_high_limit = 123
+with open("public.txt", encoding="UTF-8") as file_input:
+    lines = file_input.readlines()
 
-input_line = "Hello, world!"
 output_line = ""
 
-for letter in input_line:
-    if letter.isupper():
-        if ord(letter) + pitch < upper_low_limit:
-            output_line += chr(upper_high_limit - (pitch - (ord(letter) - upper_low_limit)))
+for input_line in lines:
+
+    for letter in input_line:
+        if letter.isupper():
+            if ord(letter) + pitch < upper_limits["low"]:
+                oversize = -pitch - (ord(letter) - upper_limits["low"])
+                output_line += chr(upper_limits["hi"] - oversize)
+            elif ord(letter) + pitch > upper_limits["hi"]:
+                oversize = pitch - (upper_limits["hi"] - ord(letter))
+                output_line += chr(upper_limits["low"] + oversize)
+            else:
+                output_line += chr(ord(letter) + pitch)
+        elif letter.islower():
+            if ord(letter) + pitch < lower_limits["low"]:
+                oversize = -pitch - (ord(letter) - lower_limits["low"])
+                output_line += chr(lower_limits["hi"] - oversize)
+            elif ord(letter) + pitch > lower_limits["hi"]:
+                oversize = pitch - (lower_limits["hi"] - ord(letter))
+                output_line += chr(lower_limits["low"] + oversize)
+            else:
+                output_line += chr(ord(letter) + pitch)
         else:
-            output_line += chr(ord(letter) + pitch)
+            output_line += letter
+    output_line =+ "\n"
+
+with open("private.txt", "w", encoding="UTF-8") as file_out:
+    file_out.writelines(output_line)
