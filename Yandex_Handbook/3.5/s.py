5fed8d19760806661288b58541fd924d190bf36a
kurskov <dmitrii@kurskov.ru>
2023-09-18 20:54:58 +0300

    Lesson 3.5 S

--- Yandex_Handbook/3.5/s.py
+++ Yandex_Handbook/3.5/s.py
@@ -1,7 +1,26 @@
-pitch = int(input())
+"""Это будет наш секрет
 
-upper_limits = {"low": 65, "hi": 91}
-lower_limits = {"low": 97, "hi": 123}
+Шифр Цезаря, также известный как шифр сдвига, код Цезаря — один
+из самых простых и наиболее широко известных методов шифрования.
+Он назван в честь римского полководца Гая Юлия Цезаря, использовавшего
+его для секретной переписки со своими генералами.
+
+Давайте реализуем эту систему шифрования. Однако для простоты мы
+будем сдвигать только латинские символы по кругу.
+
+Формат ввода
+Вводится размер сдвига для шифрования.
+В файле public.txt содержится текст на английском языке.
+
+Формат вывода
+В файл private.txt запишите зашифрованный текст.
+"""
+
+pitch = int(input())
+pitch %= 26
+
+upper_limits = {"low": 65, "hi": 90}
+lower_limits = {"low": 97, "hi": 122}
 
 with open("public.txt", encoding="UTF-8") as file_input:
     lines = file_input.readlines()
@@ -14,24 +33,23 @@ for input_line in lines:
         if letter.isupper():
             if ord(letter) + pitch < upper_limits["low"]:
                 oversize = -pitch - (ord(letter) - upper_limits["low"])
-                output_line += chr(upper_limits["hi"] - oversize)
+                output_line += chr(upper_limits["hi"] - oversize + 1)
             elif ord(letter) + pitch > upper_limits["hi"]:
                 oversize = pitch - (upper_limits["hi"] - ord(letter))
-                output_line += chr(upper_limits["low"] + oversize)
+                output_line += chr(upper_limits["low"] + oversize - 1)
             else:
                 output_line += chr(ord(letter) + pitch)
         elif letter.islower():
             if ord(letter) + pitch < lower_limits["low"]:
                 oversize = -pitch - (ord(letter) - lower_limits["low"])
-                output_line += chr(lower_limits["hi"] - oversize)
+                output_line += chr(lower_limits["hi"] - oversize + 1)
             elif ord(letter) + pitch > lower_limits["hi"]:
                 oversize = pitch - (lower_limits["hi"] - ord(letter))
-                output_line += chr(lower_limits["low"] + oversize)
+                output_line += chr(lower_limits["low"] + oversize - 1)
             else:
                 output_line += chr(ord(letter) + pitch)
         else:
             output_line += letter
-    output_line =+ "\n"
 
 with open("private.txt", "w", encoding="UTF-8") as file_out:
     file_out.writelines(output_line)
