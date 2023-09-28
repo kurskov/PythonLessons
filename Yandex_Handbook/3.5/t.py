370ec8ab19aa2e94cb46fad9a5ef65fb6d2ce319
kurskov <dmitrii@kurskov.ru>
2023-09-28 10:41:34 +0300

    Lesson 3.5 project T

--- Yandex_Handbook/3.5/t.py
+++ Yandex_Handbook/3.5/t.py
@@ -1,7 +1,9 @@
-with open("numbers.num", encoding="UTF-8") as file_input:
-    lines = file_input.readlines()
+lines = [b'\x00\x01\x00\x02\x00\x03\x00\x04\x00\x05'.hex()]
+
+# with open("numbers.num", encoding="UTF-8") as file_input:
+#     lines = file_input.readlines()
 
 file_sum = 0
 
 for line in lines:
-    pass
+    print(line)
