184b46f41463b0c9882b9bff9fffd3fd417719c6
kurskov <dmitrii@kurskov.ru>
2024-11-11 14:28:06 +0300

    Correct with PEP 8

--- Stepik_Python_in_tasks/2.2/01.py
+++ Stepik_Python_in_tasks/2.2/01.py
@@ -2,13 +2,13 @@ import math
 
 
 def compute_population(t):
-    
+
     population = (172 / 45) * (math.pi / 2 - math.atan((2000 - t) / 45))
-    
+
     return population
 
 
-time_start= int(input("Введите начало диапазона поиска: "))
+time_start = int(input("Введите начало диапазона поиска: "))
 time_stop = int(input("Введите конец диапазона поиска: "))
 
 for x in range(time_start, time_stop + 1):
